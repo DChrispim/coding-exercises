196d18414781456145143855220a4a3ff0e157c4
DChrispim <brenoadsdc@gmail.com>
2024-01-10 22:23:41 -0300

    Código do Desenvolvimento Extra 5.1

--- HTML-CSS-Javascript/port-BR/primeiro_javascrip/js/script.js
+++ HTML-CSS-Javascript/port-BR/primeiro_javascrip/js/script.js
@@ -13,3 +13,23 @@ const mensagemSucesso3 = () => console.log("Conexão feita com sucesso!")
 mensagemSucesso1()
 mensagemSucesso2()
 mensagemSucesso3()
+
+let titulo = document.getElementById("titulo")
+titulo.innerText = "Titulo da página adicionado com JavaScript"
+
+let link = document.querySelector("a")
+link.innerText = "Link adicionado com JavaScript"
+
+let listNaoOrdenada = document.querySelector("ul")
+listNaoOrdenada.innerHTML = `
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+`
+
+let listOrdenada = document.getElementById('lista-ordenada')
+listOrdenada.innerHTML = `
+    <li><a href="https://prozeducacao.com.br">Link</a></li>
+        <li><a href="https://prozeducacao.com.br">Link</a></li>
+        <li><a href="https://prozeducacao.com.br">Link</a></li>
+`
